c560eff30ac9306e32132cb4276a0cff6525853e
Pahaz Blinov <pahaz.blinov@gmail.com>
2015-04-05 04:25:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ __author__ = 'pahaz'
 
 def application(environ, start_response):
     assert environ.get('PATH_INFO') is not None, "environ['PATH_INFO'] is None"
-    
+
     status = "200 OK"
     headers = [('Content-type', 'text/html; charset=utf-8')]
     body = """<!DOCTYPE html>
     <h1>Example-mini-application</h1>
     """
-    
+
     start_response(status, headers)
     return [body.encode('utf-8')]
 
