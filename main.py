6a15b2406e1c376e258f6594465e639169e10a2c
Pahaz Blinov <pahaz.blinov@gmail.com>
2015-04-05 04:21:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ __author__ = 'pahaz'
 
 
 def application(environ, start_response):
+    assert environ.get('PATH_INFO') is not None, "environ['PATH_INFO'] is None"
+    
     status = "200 OK"
     headers = [('Content-type', 'text/html; charset=utf-8')]
     body = """<!DOCTYPE html>
     <h1>Example-mini-application</h1>
     """
+    
     start_response(status, headers)
     return [body.encode('utf-8')]
 
 
 if __name__ == "__main__":
     from wsgiref.simple_server import make_server
-    port = 31338
-    print("It's work! Visit http://localhost:{0}/".format(port))
-    httpd = make_server('', port, application)
-    httpd.serve_forever()
+    PORT = 31338
+    print("It's work! Visit http://localhost:{0}/".format(PORT))
+    HTTPD = make_server('', PORT, application)
+    HTTPD.serve_forever()
